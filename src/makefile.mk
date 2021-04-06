8d386affb88d73db776955bdf99b64f5839c8008
Shubham <shubhamgupto@gmail.com>
2021-04-06 09:24:55 +0530

    new naming convention

--- src/makefile.mk
+++ src/makefile.mk
@@ -1,10 +1,12 @@
-a.out : client.o iterator.o doubly_linked_list.o array_iterator.o
-	gcc -g client.o iterator.o doubly_linked_list.o array_iterator.o
+a.out : client.o iterator.o doubly_linked_list.o iterator_array.o
+	gcc -g client.o iterator.o doubly_linked_list.o iterator_array.o
 client.o : client.c iterator.h
 	gcc -c -g client.c
 iterator.o : iterator.c iterator.h
 	gcc -c -g iterator.c
-array_iterator.o : array_iterator.c array_iterator.h
-	gcc -c -g array_iterator.c 
+iterator_array.o : iterator_array.c iterator_array.h
+	gcc -c -g iterator_array.c 
 doubly_linked_list.o : doubly_linked_list.c doubly_linked_list.h
 	gcc -c -g doubly_linked_list.c
+iterator_list.o : iterator_list.c iterator_list.h
+	gcc -c -g iterator_list.c
