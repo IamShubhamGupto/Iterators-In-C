452a9e3255726e9de2bae58c3c0e6f2aa4752930
Shubham <shubhamgupto@gmail.com>
2021-05-07 11:53:26 +0530

    added [tree]delete node and [list] insert, need to test

--- src/data_structures/doubly_linked_list.c
+++ src/data_structures/doubly_linked_list.c
@@ -3,47 +3,67 @@
 #include <stdio.h>
 #include "doubly_linked_list.h"
 #define DEBUG 0
-static node_t* init_node(int value_){
-    node_t* temp = malloc(sizeof(node_t));
+static node_t *init_node(int value_)
+{
+    node_t *temp = malloc(sizeof(node_t));
     temp->value_ = value_;
     temp->next_ = NULL;
     temp->prev_ = NULL;
     return temp;
 }
 #if 1
-static void push_front(list_t* list, int value_){
-    node_t* temp = init_node(value_);
+static void push_front(list_t *list, int value_)
+{
+    node_t *temp = init_node(value_);
     temp->next_ = list->head_;
-    if(list->head_ != NULL){
+    if (list->head_ != NULL)
+    {
         list->head_->prev_ = temp;
-    }else{
+    }
+    else
+    {
         list->tail_ = temp;
     }
     list->head_ = temp;
 }
-static void push_back(list_t* list, int value_){
-    node_t* temp = init_node(value_);
+static void push_back(list_t *list, int value_)
+{
+    node_t *temp = init_node(value_);
     //no element
-    if(list->head_ == NULL){
+    if (list->head_ == NULL)
+    {
         list->head_ = temp;
         list->tail_ = temp;
     }
     temp->prev_ = list->tail_;
     list->tail_->next_ = temp;
-    list->tail_ = temp; 
+    list->tail_ = temp;
 }
-static void insert(list_t* list, int value_, int position){
-
+static void insert(list_t *list, int value_, int position)
+{
+    node_t *temp = list->head_;
+    node_t *newNode = init_node(value_);
+    for (int i = 0; i < position - 1; i++)
+    {
+        temp = temp->next_;
+        //printf("%d\n",temp->value_);
+    }
+    newNode->next_ = temp->next_;
+    (temp->next_)->prev_ = newNode;
+    newNode->prev_ = temp;
 }
-static void pop_back(list_t* list){
-    node_t* head = list->head_;
-    node_t* tail = list->tail_;
+static void pop_back(list_t *list)
+{
+    node_t *head = list->head_;
+    node_t *tail = list->tail_;
     //no element
-    if(head == NULL){
+    if (head == NULL)
+    {
         return;
     }
     //1 element
-    if(head == tail){
+    if (head == tail)
+    {
         list->head_ = NULL;
         list->tail_ = NULL;
         free(head);
@@ -60,21 +80,25 @@ static void pop_back(list_t* list){
     // free(head);
     // head = NULL;
 }
-static void pop_front(list_t* list){
-    node_t* head = list->head_;
+static void pop_front(list_t *list)
+{
+    node_t *head = list->head_;
     //no element
-    if(head == NULL){
+    if (head == NULL)
+    {
         return;
     }
     list->head_ = list->head_->next_;
     free(head);
     head = NULL;
 }
-static void erase(list_t* list, int position){
-    node_t* current = list->head_;
-    node_t* previous = NULL;
+static void erase(list_t *list, int position)
+{
+    node_t *current = list->head_;
+    node_t *previous = NULL;
     int i = 0;
-    while(i != position){
+    while (i != position)
+    {
         ++i;
         previous = current;
         current = current->next_;
@@ -87,32 +111,36 @@ static void erase(list_t* list, int position){
     current = NULL;
 }
 
-static void display_list(list_t* list){
-    node_t* current = list->head_;
-    while(current != NULL){
-        printf("value = %d\t",current->value_);
-        if(DEBUG){
-            if(current->next_ != NULL)
-                printf("next value = %d\t",current->next_->value_);
-            if(current->prev_ != NULL)
-                printf("prev value = %d\n",current->prev_->value_);
-            else    
+static void display_list(list_t *list)
+{
+    node_t *current = list->head_;
+    while (current != NULL)
+    {
+        printf("value = %d\t", current->value_);
+        if (DEBUG)
+        {
+            if (current->next_ != NULL)
+                printf("next value = %d\t", current->next_->value_);
+            if (current->prev_ != NULL)
+                printf("prev value = %d\n", current->prev_->value_);
+            else
                 printf("\n");
         }
-            
+
         current = current->next_;
     }
     printf("\n");
 }
 #endif
 
-static list_t* init_list(){
-    list_t* temp = malloc(sizeof(list_t));
+static list_t *init_list()
+{
+    list_t *temp = malloc(sizeof(list_t));
     temp->head_ = NULL;
     temp->tail_ = NULL;
-    #if 0
+#if 0
     temp->ptr_ = &vtbl_list;
     temp->ptr_->self = temp;
-    #endif
+#endif
     return temp;
 }
--- src/data_structures/tree.c
+++ src/data_structures/tree.c
@@ -6,8 +6,9 @@
 
 #define DEBUG 0
 
-static tree_node_t* init_node(const int value_){
-    tree_node_t* temp = (tree_node_t*)malloc(sizeof(tree_node_t));
+static tree_node_t *init_node(const int value_)
+{
+    tree_node_t *temp = (tree_node_t *)malloc(sizeof(tree_node_t));
     temp->value_ = value_;
     temp->left_ = NULL;
     temp->right_ = NULL;
@@ -15,87 +16,23 @@ static tree_node_t* init_node(const int value_){
     return temp;
 }
 
-static tree_t* init_tree(){
-    tree_t* temp = (tree_t*)malloc(sizeof(tree_t));
+static tree_t *init_tree()
+{
+    tree_t *temp = (tree_t *)malloc(sizeof(tree_t));
     temp->root_ = NULL;
     return temp;
 }
 #define DEBUG_IN 0
-static void insert(tree_t* tree, const int new){
-    tree_node_t* current = tree->root_;
-    tree_node_t* previous = NULL;
-    tree_node_t* new_node = init_node(new);
+static void insert(tree_t *tree, const int new)
+{
+    tree_node_t *current = tree->root_;
+    tree_node_t *previous = NULL;
+    tree_node_t *new_node = init_node(new);
 
-    while(current != NULL){
+    while (current != NULL)
+    {
         previous = current;
-        if(new < current->value_){
-            current = current->left_;
-        }else{
-            current = current->right_;
-        }
-    }
-    new_node->parent_ = previous;
-    if(previous == NULL){
-        if(DEBUG_IN){
-            printf("[insert] root\n");
-        }
-        new_node->parent_ = NULL;
-        tree->root_ = new_node;
-
-    }else if (new < previous->value_){
-        if(DEBUG_IN){
-            printf("[insert] left\n");
-        }
-        previous->left_ = new_node;
-    }else{
-        if(DEBUG_IN){
-            printf("[insert] right\n");
-        }
-        previous->right_ = new_node;
-    }
-}
-static int is_empty(const tree_t* tree){
-    return tree->root_ == NULL;
-}
-static void erase(tree_t* tree, const int value){
-    // tree_node_t* current = tree->root_;
-    // tree_node_t* previous = NULL;
-    // while(current && current->value_ != value){
-    //     previous = current;
-    //     if(value < current->value_){
-    //         current = current->left_;
-    //     }else{
-    //         current = current->right_;
-    //     }
-    // }
-
-    // //delete_node(tree->root_, current, previous);
-
-    // if(current == NULL){
-    //     printf("Key not found!!\n");
-    //     return;
-    // }else if(current->left_ == NULL || current->right_ == NULL){
-    //     if(current != tree->root_){
-    //         if(previous->left_ == current){
-    //             previous->left_ = NULL;
-    //         }else{
-    //             previous->right_ = NULL;
-    //         }
-    //     }else{
-    //         tree->root_ = NULL;
-    //     }
-    //     free(current);
-    //     current = NULL;
-    // }else if(current->left_ && current->right_){
-    //     tree_node_t* temp = in_order_successor(current->right_);
-
-    // }
-}
-
-static tree_node_t* find(const tree_t* tree, const int value){
-    tree_node_t* current = tree->root_;
-    while(current && current->value_ != value){
-        if(value < current->value_)
+        if (new < current->value_)
         {
             current = current->left_;
         }
@@ -104,81 +41,248 @@ static tree_node_t* find(const tree_t* tree, const int value){
             current = current->right_;
         }
     }
-    if(current && current->value_ == value)
+    new_node->parent_ = previous;
+    if (previous == NULL)
+    {
+        if (DEBUG_IN)
+        {
+            printf("[insert] root\n");
+        }
+        new_node->parent_ = NULL;
+        tree->root_ = new_node;
+    }
+    else if (new < previous->value_)
+    {
+        if (DEBUG_IN)
+        {
+            printf("[insert] left\n");
+        }
+        previous->left_ = new_node;
+    }
+    else
+    {
+        if (DEBUG_IN)
+        {
+            printf("[insert] right\n");
+        }
+        previous->right_ = new_node;
+    }
+}
+static int is_empty(const tree_t *tree)
+{
+    return tree->root_ == NULL;
+}
+static void erase(tree_t *tree, const int value)
+{
+    tree_node_t *current = tree->root_;
+    tree_node_t *previous = NULL;
+    while (current && current->value_ != value)
+    {
+        previous = current;
+        if (value < current->value_)
+        {
+            current = current->left_;
+        }
+        else
+        {
+            current = current->right_;
+        }
+    }
+
+    //delete_node(tree->root_, current, previous);
+
+    if (current == NULL)
+    {
+        printf("Key not found!!\n");
+        return;
+    }
+
+    if (current->left_ == NULL || current->right_ == NULL)
+    {
+        tree_node_t *newcurr;
+        if (current->left_ == NULL)
+            newcurr = current->right_;
+        else
+            newcurr = current->left_;
+
+        if (previous == NULL)
+        {
+            if (current->left_)
+            {
+                tree_node_t *max = get_rightmost_leaf(current->left_);
+                max->left_ = current->left_;
+                max->parent_ = NULL;
+                max->right_ = current->right_;
+                current->right_->parent_ = max;
+            }
+            else
+            {
+                tree->root_ = current->right_;
+                current->right_->parent_ = NULL;
+            }
+            free(current);
+            current = NULL;
+            return;
+        }
+        if (current == previous->left_)
+        {
+            previous->left_ = newcurr;
+        }
+        else
+        {
+            previous->right_ = newcurr;
+        }
+        free(current);
+        current = NULL;
+    }
+    else
+    {
+        tree_node_t *parent = NULL;
+        tree_node_t *temp = current->right_;
+        while (temp->left_ != NULL)
+        {
+            parent = temp;
+            temp = temp->left_;
+        }
+        // check if the parent of the inorder
+        // successor is the curr or not(i.e. curr=
+        // the node which has the same data as
+        // the given data by the user to be
+        // deleted). if it isn't, then make the
+        // the left child of its parent equal to
+        // the inorder successor'd right child.
+        if (parent)
+            parent->left_ = temp->right_;
+        // if the inorder successor was the
+        // curr (i.e. curr = the node which has the
+        // same data as the given data by the
+        // user to be deleted), then make the
+        // right child of the node to be
+        // deleted equal to the right child of
+        // the inorder successor.
+        else
+            current->right_ = temp->right_;
+        current->value_ = temp->value_;
+        free(current);
+        current = NULL;
+    }
+
+    // if (current->left_ == NULL || current->right_ == NULL)
+    // {
+    //     if (current != tree->root_)
+    //     {
+    //         if (previous->left_ == current)
+    //         {
+    //             previous->left_ = NULL;
+    //         }
+    //         else
+    //         {
+    //             previous->right_ = NULL;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         tree->root_ = NULL;
+    //     }
+    //     free(current);
+    //     current = NULL;
+    // }
+    // else if (current->left_ && current->right_)
+    // {
+    //     tree_node_t *temp = in_order_successor(current->right_);
+    // }
+}
+
+static tree_node_t *find(const tree_t *tree, const int value)
+{
+    tree_node_t *current = tree->root_;
+    while (current && current->value_ != value)
+    {
+        if (value < current->value_)
+        {
+            current = current->left_;
+        }
+        else
+        {
+            current = current->right_;
+        }
+    }
+    if (current && current->value_ == value)
     {
         return current;
     }
     return NULL;
-
 }
 
 #define DEBUG_PT 0
-static void print_tree(const tree_t* tree) 
+static void print_tree(const tree_t *tree)
 {
-  if(is_empty(tree))
-  {
-    printf("Empty tree !!\n");
-    return;
-  }
-  tree_node_t* root = tree->root_;
-  if(DEBUG_PT)
-  {
-    printf("[printTree]current root is %d\n", root->value_ );
-  }
-  print_in_order(root, "", 0);
+    if (is_empty(tree))
+    {
+        printf("Empty tree !!\n");
+        return;
+    }
+    tree_node_t *root = tree->root_;
+    if (DEBUG_PT)
+    {
+        printf("[printTree]current root is %d\n", root->value_);
+    }
+    print_in_order(root, "", 0);
 }
 
-static void print_in_order(tree_node_t* root, char* prefix, int isLeft)
+static void print_in_order(tree_node_t *root, char *prefix, int isLeft)
 {
     //char left[3] = "   ";
     //char right[3] = "│  ";
-    if(root != NULL)
+    if (root != NULL)
     {
-        char prefix2[strlen(prefix)+5];
+        char prefix2[strlen(prefix) + 5];
         strcpy(prefix2, prefix);
-        printf("%s",prefix);
+        printf("%s", prefix);
         printf(isLeft ? "├──" : "└──");
-        printf("%d\n",root->value_);
+        printf("%d\n", root->value_);
         // if(isLeft){
         //     strcat(prefix2,left);
         // }else{
         //     strcat(prefix2,right);
         // }
-        strcat(prefix2,(isLeft ? "│  " : "   "));
-        print_in_order(root->left_, prefix2 , 1);
+        strcat(prefix2, (isLeft ? "│  " : "   "));
+        print_in_order(root->left_, prefix2, 1);
         print_in_order(root->right_, prefix2, 0);
-  }
+    }
 }
 
-static tree_node_t* get_leftmost_leaf(tree_node_t* root)
+static tree_node_t *get_leftmost_leaf(tree_node_t *root)
 {
-    while(root && root->left_ != NULL)
+    while (root && root->left_ != NULL)
     {
         root = root->left_;
     }
     return root;
 }
 
-static tree_node_t* get_rightmost_leaf(tree_node_t* root)
+static tree_node_t *get_rightmost_leaf(tree_node_t *root)
 {
-    while(root && root->right_ != NULL)
+    while (root && root->right_ != NULL)
     {
         root = root->right_;
     }
     return root;
 }
 
-static tree_node_t* in_order_successor(tree_node_t* node){
-    tree_node_t* parent = NULL;
-    if(node == NULL)
+static tree_node_t *in_order_successor(tree_node_t *node)
+{
+    tree_node_t *parent = NULL;
+    if (node == NULL)
     {
         return node;
     }
-    if(node->right_ != NULL)
+    if (node->right_ != NULL)
     {
         node = node->right_;
         node = get_leftmost_leaf(node);
-    }else
+    }
+    else
     {
         parent = node->parent_;
         while (parent != NULL && node == parent->right_)
@@ -191,17 +295,19 @@ static tree_node_t* in_order_successor(tree_node_t* node){
     return node;
 }
 
-static tree_node_t* in_order_predecessor(tree_node_t* node){
-    tree_node_t* parent = NULL;
-    if(node == NULL)
+static tree_node_t *in_order_predecessor(tree_node_t *node)
+{
+    tree_node_t *parent = NULL;
+    if (node == NULL)
     {
         return node;
     }
-    if(node->left_ != NULL)
+    if (node->left_ != NULL)
     {
         node = node->left_;
         node = get_rightmost_leaf(node);
-    }else
+    }
+    else
     {
         parent = node->parent_;
         while (parent != NULL && node == parent->left_)
